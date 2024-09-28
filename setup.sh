d00fce39d8e7673539639f9fec0154072c50a533
mEsUsah <stanley@haxor.no>
2024-09-28 21:20:13 +0200

    fix: add setup script

---
+++ setup.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Written by Stanley Skarshaug 2024.09.29
+# https://github.com/mEsUsah
+# https://www.haxor.no
+
+# Install PHP dependencies
+composer upgrade vlucas/phpdotenv:^5
+composer require phpunit/phpunit --dev
+
+# Create directories
+mkdir -p tests/Unit
+mkdir -p tests/Feature
+
+# Config file
+wget https://raw.githubusercontent.com/mEsUsah/craft-phpunit/refs/heads/master/phpunit.xml -O phpunit.xml
+
+# Bootstrap file
+wget https://raw.githubusercontent.com/mEsUsah/craft-phpunit/refs/heads/master/tests/bootstrap.php -O tests/bootstrap.php
+
+# Examples
+wget https://raw.githubusercontent.com/mEsUsah/craft-phpunit/refs/heads/master/tests/Feature/ExampleFeatureTest.php -O tests/Feature/ExampleFeatureTest.php
+wget https://raw.githubusercontent.com/mEsUsah/craft-phpunit/refs/heads/master/tests/Unit/ExampleUnitTest.php -O tests/Unit/ExampleUnitTest.php
