ded4498aa69c9b1e045de3abc0ada573b0c14af1
Stanley Skarshaug <43819369+mEsUsah@users.noreply.github.com>
2024-11-13 14:18:48 +0100

    Update ArticleTest.php

--- tests/Feature/ArticleTest.php
+++ tests/Feature/ArticleTest.php
@@ -11,7 +11,7 @@ use GuzzleHttp\Client;
 
 final class ArticlesTest extends TestCase
 {    
-    public function test_can_access_all_articles(): void
+    public function test_can_access_articles_in_section(): void
     {
         $client = new Client([
             'base_uri' =>  App::env('PRIMARY_SITE_URL'),
@@ -29,4 +29,35 @@ final class ArticlesTest extends TestCase
             $this->assertEquals(200, $response->getStatusCode());
         }
     }
+
+    public function test_can_access_all_articles_with_url(): void
+    {
+        $client = new Client([
+            'base_uri' =>  App::env('PRIMARY_SITE_URL'),
+            'timeout'  => 10.0,
+            'verify' => false
+        ]);
+
+        $sites = Craft::$app->getSites()->getAllSites();
+        
+        foreach ($sites as $site) {
+            if($site->handle == 'default') {
+                continue;
+            }
+
+            $entries = Entry::find()->site($site->handle)->all();
+            foreach ($entries as $entry) {
+                $route = $entry->getUrl();
+                if($route == null) {
+                    continue;
+                }
+
+                $response = $client->request('GET', $route);
+                
+                $this->assertEquals(200, $response->getStatusCode());
+                $this->assertNotNull($entry->title);
+            }
+        }
+    }
+    
 }
