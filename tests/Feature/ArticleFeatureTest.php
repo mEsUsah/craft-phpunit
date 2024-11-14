fe8921dad194ee0e7a965f948b6ed040d0dab12d
Stanley Skarshaug <43819369+mEsUsah@users.noreply.github.com>
2024-11-14 11:50:43 +0100

    fix(ArticleFeatureTest): remove deprecated test

--- tests/Feature/ArticleFeatureTest.php
+++ tests/Feature/ArticleFeatureTest.php
@@ -1,6 +1,6 @@
 <?php
 /*
-    Written by Stanley Skarshaug 2024.09.29
+    Written by Stanley Skarshaug 2024.11.14
     https://www.haxor.no
 */
 
@@ -8,28 +8,11 @@ use PHPUnit\Framework\TestCase;
 use craft\elements\Entry;
 use craft\helpers\App;
 use GuzzleHttp\Client;
+use GuzzleHttp\Pool;
+use GuzzleHttp\Psr7\Request;
 
-final class ArticlesTest extends TestCase
+final class ArticleFeatureTest extends TestCase
 {    
-    public function test_can_access_articles_in_section(): void
-    {
-        $client = new Client([
-            'base_uri' =>  App::env('PRIMARY_SITE_URL'),
-            'timeout'  => 4.0,
-        ]);
-        
-        $entries = Entry::find()
-            ->section(["articles"])
-            ->all();
-
-        foreach ($entries as $entry) {
-            $route = $entry->getUrl();
-            $response = $client->request('GET', $route);
-            
-            $this->assertEquals(200, $response->getStatusCode());
-        }
-    }
-
     public function test_can_access_all_articles_with_url(): void
     {
         $client = new Client([
@@ -43,10 +26,6 @@ final class ArticlesTest extends TestCase
         
         // Loop through all sites and get all entries
         foreach ($sites as $site) {
-            if($site->handle == 'default') {
-                continue;
-            }
-
             $siteEntries = Entry::find()->site($site->handle)->all();
             array_push($entries, ...$siteEntries);
         }
